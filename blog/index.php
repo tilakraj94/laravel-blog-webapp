cddfbca4491bf954ef859448a14beb3c1a74f2fa
BOTjr <tilakraj94@users.noreply.github.com>
2017-06-25 06:08:05 +0530

    Update index.php

--- blog/index.php
+++ blog/index.php
@@ -19,7 +19,7 @@
 |
 */
 
-require __DIR__.'/../../blogapp/bootstrap/autoload.php';
+require __DIR__.'/../blogapp/bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -33,7 +33,7 @@ require __DIR__.'/../../blogapp/bootstrap/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../../blogapp/bootstrap/app.php';
+$app = require_once __DIR__.'/../blogapp/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
